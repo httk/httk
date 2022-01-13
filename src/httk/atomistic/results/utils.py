4fdf8c2d857fdff9bf7c6f426167146c720617b1
hpleva <henrik.levamaki@gmail.com>
2022-01-13 21:18:13 +0100

    Fix problems in the aimd results objects.

--- src/httk/atomistic/results/utils.py
+++ src/httk/atomistic/results/utils.py
@@ -24,7 +24,7 @@ class ElasticTensorFloat(HttkObject):
     @httk_typed_init({'_matrix': (float, 6, 6)})
     def __init__(self, _matrix):
         """
-        Private constructor, as per httk coding guidelines. Use ElasticTensor.create instead.
+        Private constructor, as per httk coding guidelines. Use ElasticTensorFloat.create instead.
         """
         self._matrix = _matrix
 
@@ -34,9 +34,7 @@ class ElasticTensorFloat(HttkObject):
         matrix = A 6x6 array that represent the elastic constants.
         """
         if matrix is None:
-            raise Exception("ElasticTensor.create: The elastic constants matrix must be given as a 6x6 array.")
-        # else:
-        #     matrix = FracVector.use(matrix)
+            raise Exception("ElasticTensorFloat.create: The elastic constants matrix must be given as a 6x6 array of floats.")
         return cls(matrix)
 
     @property
@@ -44,13 +42,25 @@ class ElasticTensorFloat(HttkObject):
         return list(self._matrix)
 
 class ThirdOrderElasticTensor(HttkObject):
-    @httk_typed_init({'_matrix': (float, 6, 6*6), '_shape': (int, 1, 3)})
+    @httk_typed_init({'_matrix': (float, 6*6, 6), '_shape': (int, 1, 3)})
     def __init__(self, _matrix, _shape):
         """
         Private constructor, as per httk coding guidelines. Use ThirdOrderElasticTensor.create instead.
         """
-        self._matrix = _matrix
-        self._shape = _shape
+        # The SQLite database returns the _matrix and the _shape as a zip iterator,
+        # so for convenience we make sure that they are always normal lists here.
+        self._matrix = list(_matrix)
+        # Make sure that _matrix is always in the 2D 6x36 format.
+        if len(self._matrix[0]) == 6:
+            tmp1 = []
+            for i in range(6):
+                tmp2 = []
+                for j in range(6):
+                    for k in range(6):
+                        tmp2.append(self._matrix[i][j][k])
+                tmp1.append(tmp2)
+            self._matrix = tmp1
+        self._shape = list(_shape)
 
     @classmethod
     def create(cls, matrix=None, shape=(6,6,6)):
@@ -59,45 +69,91 @@ class ThirdOrderElasticTensor(HttkObject):
         """
         if matrix is None:
             raise Exception("ThirdOrderElasticTensor.create: The elastic constants matrix must be given as a 6x6x6 array.")
-        # else:
-        #     matrix = FracVector.use(matrix)
         return cls(matrix, shape)
 
     @property
     def matrix(self):
-        return list(self._matrix)
+        """Re-create the third dimension from the flattened 2D _matrix.
+        """
+        shape = self.shape
+        matrix = []
+        for i in range(shape[0]):
+            index_flat = 0
+            tmp1 = []
+            for j in range(shape[1]):
+                tmp2 = []
+                for k in range(shape[2]):
+                    tmp2.append(self._matrix[i][index_flat])
+                    index_flat += 1
+                tmp1.append(tmp2)
+            matrix.append(tmp1)
+        return matrix
 
     @property
     def shape(self):
-        return [x[0] for x in list(self._shape)]
+        # When returned from SQLite, each integer is wrapped in a tuple,
+        # so we check for it here.
+        if hasattr(self._shape[0], "__iter__"):
+            return [x[0] for x in self._shape]
+        else:
+            return self._shape
 
+Nmax = 2
 class PlaneDependentTensor(HttkObject):
     """Generic class for a crystal plane direction dependent quantities.
-    Goes from [000] up to [222].
+    Goes from plane [000] up to [Nmax,Nmax,Nmax].
     """
-    @httk_typed_init({'_matrix': (float, 3, 3*3), '_shape': (int, 1, 3)})
+    @httk_typed_init({'_matrix': (float, (Nmax+1)*(Nmax+1), Nmax+1), '_shape': (int, 1, Nmax+1)})
     def __init__(self, _matrix, _shape):
+        global Nmax
         """
         Private constructor, as per httk coding guidelines. Use PlaneDependentTensor.create instead.
         """
-        self._matrix = _matrix
-        self._shape = _shape
+        self._matrix = list(_matrix)
+        # Make sure that _matrix is always in the 2D 3x9 format.
+        if len(self._matrix[0]) == Nmax+1:
+            tmp1 = []
+            for i in range(Nmax+1):
+                tmp2 = []
+                for j in range(Nmax+1):
+                    for k in range(Nmax+1):
+                        tmp2.append(self._matrix[i][j][k])
+                tmp1.append(tmp2)
+            self._matrix = tmp1
+        self._shape = list(_shape)
 
     @classmethod
-    def create(cls, matrix=None, shape=(3,3,3)):
+    def create(cls, matrix=None, shape=(Nmax+1,Nmax+1,Nmax+1)):
         """
-        matrix = A 2x2x2 array that represent the elastic constants.
+        matrix = A 3x3x3 array that represent plane dependent quantities.
         """
         if matrix is None:
-            raise Exception("PlaneDependentTensor.create: The elastic constants matrix must be given as a 2x2x2 array.")
-        # else:
-        #     matrix = FracVector.use(matrix)
+            raise Exception("PlaneDependentTensor.create: The matrix must be given as a 3x3x3 array.")
         return cls(matrix, shape)
 
     @property
     def matrix(self):
-        return list(self._matrix)
+        """Re-create the third dimension from the flattened 2D _matrix.
+        """
+        shape = self.shape
+        matrix = []
+        for i in range(shape[0]):
+            index_flat = 0
+            tmp1 = []
+            for j in range(shape[1]):
+                tmp2 = []
+                for k in range(shape[2]):
+                    tmp2.append(self._matrix[i][index_flat])
+                    index_flat += 1
+                tmp1.append(tmp2)
+            matrix.append(tmp1)
+        return matrix
 
     @property
     def shape(self):
-        return [x[0] for x in list(self._shape)]
+        # When returned from SQLite, each integer is wrapped in a tuple,
+        # so we check for it here.
+        if hasattr(self._shape[0], "__iter__"):
+            return [x[0] for x in self._shape]
+        else:
+            return self._shape
