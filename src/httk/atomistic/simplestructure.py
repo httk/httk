eeb667043d7d04d05215500ab635715cb050368a
edvva488 <edvva488@student.liu.se>
2024-11-22 18:07:05 +0100

    Added more functions and constructor to simplestructure.

--- src/httk/atomistic/simplestructure.py
+++ src/httk/atomistic/simplestructure.py
@@ -7,16 +7,16 @@ from httk.atomistic.cell import Cell
 
 class SimpleStructure(HttkObject):
     def __init__(self, cell_basis=None, cell_parameters=None, cell_niggli=None, cell_metric=None, cell_volume=None, scale=1, sites_fractional=None, sites_cartesian=None, species=None, species_sites=None):
-        self.cell_basis = cell_basis
-        self.cell_parameters = cell_parameters
-        self.cell_niggli = cell_niggli
-        self.cell_metric = cell_metric
-        self.scale = scale
-        self.sites_fractional = sites_fractional
-        self.sites_cartesian = sites_cartesian
-        self.species = species
-        self.species_sites = species_sites
-        self.cell_volume = cell_volume
+        self._cell_basis = cell_basis
+        self._cell_parameters = cell_parameters
+        self._cell_niggli = cell_niggli
+        self._cell_metric = cell_metric
+        self._scale = scale
+        self._sites_fractional = sites_fractional
+        self._sites_cartesian = sites_cartesian
+        self._species = species
+        self._species_sites = species_sites
+        self._cell_volume = cell_volume
     
     @classmethod
     def create(cls, cell_basis=None, cell_parameters=None, cell_niggli=None, cell_metric=None, scale=1, sites_fractional=None, sites_cartesian=None, species=None, species_sites=None):
@@ -35,10 +35,40 @@ class SimpleStructure(HttkObject):
                     i += 1
                 if not all_vectors_correct:
                     print("cell_basis error")
-        if cell_parameters:
+            cell_volume = 1 # Some calculation here
+        elif cell_parameters:
             if type(cell_parameters) is list and len(cell_parameters) == 6:
                 cell_parameters = {"a": cell_parameters[0], "b": cell_parameters[1], "c": cell_parameters[2], "alpha": cell_parameters[3], "beta": cell_parameters[4], "gamma": cell_parameters[5]}
             if type(cell_parameters) is not dict:
                 print("cell_parameters error")
-        
+            cell_volume = 1 # Some calculation here
+        elif cell_niggli:
+            cell_volume = 1 # Some calculation here
+        elif cell_metric:
+            cell_volume = 1 # Some calculation here
+        if sites_fractional is None and sites_cartesian is None:
+            print("provide sites!")
+        if species_sites is None and species is not None:
+            species_sites = []
+            for k, v in species:
+                species_sites += [k]*v
+        if species is None and species_sites is not None:
+            species = {}
+            for atom in species_sites:
+                if atom not in species:
+                    species[atom] = 1
+                else:
+                    species[atom] += 1
         return cls(cell_basis, cell_parameters, cell_niggli, cell_metric, cell_volume, scale, sites_fractional, sites_cartesian, species, species_sites)
+    
+    @property
+    def sites_cartesian(self):
+        if self._sites_cartesian is None:
+            self._sites_cartesian = (self._cell_basis.T @ self._sites_fractional.T).T
+        return self._sites_cartesian
+
+    @property
+    def sites_fractional(self):
+        if self._sites_fractional is None:
+            self._sites_fractional = self._cell_basis.inv() @ self._sites_cartesian
+        return self._sites_fractional
