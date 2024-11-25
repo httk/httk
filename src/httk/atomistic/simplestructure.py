6537c090c8e67e582d8c421a326902c01c9831a1
edvva488 <edvva488@student.liu.se>
2024-11-25 13:47:33 +0100

    Added functions to simplestructure, removed self._volume.

--- src/httk/atomistic/simplestructure.py
+++ src/httk/atomistic/simplestructure.py
@@ -1,12 +1,9 @@
 import numpy as np
 from httk.core.basic import *
-from httk.core.httkobject import HttkObject, HttkPluginPlaceholder, httk_typed_property, httk_typed_property_resolve, httk_typed_property_delayed, httk_typed_init
-from httk.atomistic.assignments import Assignments
-from httk.atomistic.representativesites import RepresentativeSites
-from httk.atomistic.cell import Cell
+from httk.core.httkobject import HttkObject
 
 class SimpleStructure(HttkObject):
-    def __init__(self, cell_basis=None, cell_parameters=None, cell_niggli=None, cell_metric=None, volume=None, scale=1, sites_fractional=None, sites_cartesian=None, species=None, species_sites=None):
+    def __init__(self, cell_basis=None, cell_parameters=None, cell_niggli=None, cell_metric=None, scale=None, sites_fractional=None, sites_cartesian=None, species=None, species_sites=None):
         self._cell_basis = cell_basis
         self._cell_parameters = cell_parameters
         self._cell_niggli = cell_niggli
@@ -16,10 +13,9 @@ class SimpleStructure(HttkObject):
         self._sites_cartesian = sites_cartesian
         self._species = species
         self._species_sites = species_sites
-        self._volume = volume
     
     @classmethod
-    def create(cls, cell_basis=None, cell_parameters=None, cell_niggli=None, cell_metric=None, scale=1, sites_fractional=None, sites_cartesian=None, species=None, species_sites=None):
+    def create(cls, cell_basis=None, cell_parameters=None, cell_niggli=None, cell_metric=None, scale=None, sites_fractional=None, sites_cartesian=None, species=None, species_sites=None):
         if cell_basis:
             if type(cell_basis) is not list:
                 cell_basis = list(cell_basis)
@@ -35,7 +31,6 @@ class SimpleStructure(HttkObject):
                     i += 1
                 if not all_vectors_correct:
                     print("cell_basis error")
-            volume = abs(cell_basis.det())
         else:
             if cell_parameters:
                 if type(cell_parameters) is list and len(cell_parameters) == 6:
@@ -55,7 +50,7 @@ class SimpleStructure(HttkObject):
                     species[atom] = 1
                 else:
                     species[atom] += 1
-        return cls(cell_basis, cell_parameters, cell_niggli, cell_metric, volume, scale, sites_fractional, sites_cartesian, species, species_sites)
+        return cls(cell_basis, cell_parameters, cell_niggli, cell_metric, scale, sites_fractional, sites_cartesian, species, species_sites)
 
     @property
     def sites_cartesian(self):
@@ -68,9 +63,38 @@ class SimpleStructure(HttkObject):
         if self._sites_fractional is None:
             self._sites_fractional = self._cell_basis.inv() @ self._sites_cartesian
         return self._sites_fractional
+
+    @property
+    def scale(self):
+        if self._scale is None:
+            if self._scale is not None:
+                self._scale = abs(self._volume / self._cell_basis.det()) # Some calculation here
+        return self._scale
     
     @property
-    def volume(self):
-        if self._volume is None:
-            self._volume = abs(self.basis.det()) # Some calculation here
-        return self._volume
+    def cell_basis(self):
+        return self._cell_basis
+    
+    @property
+    def cell_parameters(self):
+        if self._cell_parameters is None:
+            self._cell_parameters = self._cell_basis # some transformation function
+        return self._cell_parameters
+    
+    @property
+    def cell_niggli(self):
+        if self._cell_niggli is None:
+            self._cell_niggli = self._cell_basis # some transformation function
+        return self._cell_niggli
+    
+    @property
+    def cell_metric(self):
+        if self._cell_metric is None:
+            self._cell_metric = self._cell_basis # some transformation function
+        return self._cell_metric
+    
+    def get_volume(self):
+        return abs(self._scale * self._cell_basis.det()) # Some calculation here
+    
+    def get_formula(self):
+        return self._species # some calculation here
